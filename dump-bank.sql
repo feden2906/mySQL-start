8278925cfe8df48c6e60dfebc150710d89494401
Fedenko Maksym <67603506+feden2906@users.noreply.github.com>
2021-02-13 22:31:58 +0200

    Update dump-bank.sql

--- dump-bank.sql
+++ dump-bank.sql
@@ -14,7 +14,6 @@ CREATE TABLE IF NOT EXISTS `bank`.`department` (
   PRIMARY KEY (`idDepartment`))
 ENGINE = InnoDB;
 
-
 -- -----------------------------------------------------
 -- Table `mydb`.`Client`
 -- -----------------------------------------------------
